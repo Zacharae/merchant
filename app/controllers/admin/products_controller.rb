2db4c87d9676b16d1e5b7f07fe0862c3ed0722b8
Zacharae <zacharyxae@gmail.com>
2016-05-04 09:49:34 -0500

    redoes admin ridirect in controllers

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -28,8 +28,8 @@ class Admin::ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
-        format.json { render :show, status: :created, location: @product }
+        format.html { redirect_to admin_product_url(@product), notice: 'Product was successfully created.' }
+        format.json { render :show, status: :created, location: admin_product_url(@product) }
       else
         format.html { render :new }
         format.json { render json: @product.errors, status: :unprocessable_entity }
@@ -42,7 +42,7 @@ class Admin::ProductsController < ApplicationController
   def update
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        format.html { redirect_to admin_product_url(@product), notice: 'Product was successfully updated.' }
         format.json { render :show, status: :ok, location: @product }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class Admin::ProductsController < ApplicationController
   def destroy
     @product.destroy
     respond_to do |format|
-      format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
+      format.html { redirect_to admin_products_url, notice: 'Product was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
