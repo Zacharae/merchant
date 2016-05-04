37c632ff8c15c0b8fcaa9b2624e76d2e8e7953ea
Zacharae <zacharyxae@gmail.com>
2016-05-04 11:34:47 -0500

    debugs, stylizes & adds sample products

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -28,7 +28,7 @@ class Admin::CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to admin_catagory_url(@category), notice: 'Category was successfully created.' }
+        format.html { redirect_to admin_categories_url(@category), notice: 'Category was successfully created.' }
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new }
