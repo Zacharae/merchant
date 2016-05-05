0e38ab14721cabdf1747bae6d32a7036bf57df14
Zacharae <zacharyxae@gmail.com>
2016-05-05 09:46:25 -0500

    default img, validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,9 +5,11 @@ class Product < ActiveRecord::Base
   has_attached_file :avatar, 
   	:styles => { 
   		:medium => "400x400#", :thumb => "150x150>"}, 
-  	:default_url => "images/:style/missing.png"
+  	 	:default_url => "missing_:style.png"
 	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 	validates_attachment_size :avatar, :less_than => 5.megabytes
 
   validates :name, presence: true, length: {minimum: 2} 
+  validates :brand, presence: true
+  validates :category, presence: true
 end
