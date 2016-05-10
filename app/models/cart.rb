c063de6d60cce6120de63bf4d007e9bc04aa8fd8
Zacharae <zacharyxae@gmail.com>
2016-05-10 09:39:05 -0500

    adds math to cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,9 @@
 class Cart < ActiveRecord::Base
 	has_many :line_items, dependant: :destroy
+
+	def subtotal
+		line_items.select("SUM(quantity * price) AS sum")[0].sum 
+
+		# line_items.to_a.sum{ |item| item.total }
+	end
 end
