fcf45a84099bbf18eab53df5e2949e54cb98b31f
Zacharae <zacharyxae@gmail.com>
2016-05-23 10:29:32 -0500

    missing comma

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,8 +5,8 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ['S3_BUCKET_NAME']
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID']
+      :bucket => ['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
